0b6922989ee3a4e5415d0668eeef5221563adf74
mahditasfin <64248085+mahditasfin@users.noreply.github.com>
2025-02-23 21:18:23 -0800

    second m

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
     images: {
       domains: ["buttersystem.com", "localhost"], // Add image hosting domains if needed
     },
-    output: "export", // Ensures static output if using `next export`
     trailingSlash: true, // Useful for static exports
   };
   
