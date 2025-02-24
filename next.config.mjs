f669097ff1e12e5eb7fc5aa61b47c79c390133bb
mahditasfin <64248085+mahditasfin@users.noreply.github.com>
2025-02-23 21:17:55 -0800

    second m

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ["buttersystem.com", "localhost"], // Add image hosting domains if needed
+    },
+    output: "export", // Ensures static output if using `next export`
+    trailingSlash: true, // Useful for static exports
+  };
+  
+  export default nextConfig;
