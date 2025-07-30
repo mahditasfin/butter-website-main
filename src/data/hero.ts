421cd1f06b0605d0b9b48196817bf1aa94217512
mahditasfin <64248085+mahditasfin@users.noreply.github.com>
2025-07-30 13:32:20 -0700

    Update hero.ts

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
-    heading: 'Saving You Time To Pad The Bottom Line!',
-    subheading: 'Automating Inventory Refunding For Restaurants & Suppliers ',
+    heading: 'Refunds Made Easy!',
+    subheading: 'Automating Inventory Refunding For Restaurants & Suppliers',
     centerImageSrc: '/images/hero1.png',
 }
